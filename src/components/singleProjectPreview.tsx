c5a647befd5f100d7fcf229bc08a9f6e81e0c6c1
notnulldev <notnulldev0@gmail.com>
2023-01-26 15:56:37 +0100

    layout update

--- src/components/singleProjectPreview.tsx
+++ src/components/singleProjectPreview.tsx
@@ -7,15 +7,15 @@ type SingleProjectPreviewProps = {
 
 const SingleProjectPreview = ({description, imageUrl, title, linkUrl}: SingleProjectPreviewProps) => {
     return (
-      <div className="bg-base-100 shadow-xl w-1/3 h-[400px]">
-      <figure className="aspect-w-12 aspect-h-8">
-        <img src={imageUrl} alt={title} />
+      <div className="flex flex-col bg-base-100 shadow-xl w-1/3 h-[420px] hover:scale-105 hover:shadow-indigo-600 transition-all duration-200 group rounded-xl">
+      <figure className="aspect-w-12 aspect-h-8 rounded-xl">
+        <img src={imageUrl} alt={title} className="rounded-xl" />
         </figure>
-      <div className="p-8 flex flex-col gap-3 h-[150px] ">
-        <h2 className="text-xl font-bold">{title}</h2>
-        <p>{description}</p>
+      <div className="p-8 flex flex-col gap-3  flex-1">
+        <h2 className="text-xl font-bold flex">{title}</h2>
+        <p className="flex-1 flex ">{description}</p>
         <div className="justify-end flex">
-          <a href={linkUrl} target="blank" className="btn btn-primary">Visit</a>
+          <a href={linkUrl} target="blank" className="btn btn-primary group-hover:animate-pulse group-hover:animate-bounce">Visit</a>
         </div>
       </div>
     </div>
